b96f43679b4f310d128d61fd40ac82f99f8fa6f2
Alex Ehrnschwender <alexanderscott46@gmail.com>
2014-12-23 04:07:35 -0500

    test & fix for zstddev

--- ZSTDDEV.lua
+++ ZSTDDEV.lua
@@ -22,7 +22,15 @@ local function ZSTDDEV(key)
         sumsqrs = sumsqrs + math.pow((val - mean), 2)
     end
 
-    return math.sqrt(sumsqrs/(size - 1))
+    return math.sqrt(sumsqrs/(size))
 end
+
+--[[ @TEST
+redis.call("DEL", "test_zstddev")
+redis.call("ZADD", "test_zstddev", 2, "one", 4, "two", 4, "three", 4,
+            "four", 5, "five", 5, "six", 7, "seven", 9, "eight")
+local zstddev = ZSTDDEV("test_zstddev")
+assert(zstddev == 2)
+--]]
 
 return tostring(ZSTDDEV(KEYS[1]))
